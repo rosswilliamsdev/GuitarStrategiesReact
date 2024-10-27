c24b7b385f85080f06c2ed83608680aaf33a69f2
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2024-10-27 16:03:21 -0500

    set base for vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === "build" ? "/guitarstrategiesreact/" : "/", // Use base path only in production
-}));
+  base: "/",
+});
